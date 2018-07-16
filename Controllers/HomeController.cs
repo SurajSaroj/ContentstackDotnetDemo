911048145890d1d842f99c3f853de9b5227abae3
Suraj Saroj <suraj.saroj3455@gmail.com>
2018-07-16 15:49:48 +0530

    initial commi

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Contentstack.Core;
+using Contentstack.Core.Configuration;
+using Microsoft.Extensions.Options;
+using MVCDemo.Models;
+using MVCDemo;
+using Newtonsoft.Json.Linq;
+
+namespace MVCDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ContentstackClient _stack;
+        
+        public HomeController(ContentstackClient stack, IOptions<ContentstackOptions> contentstackOptions)
+       {
+              _stack = stack;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var query = _stack.ContentType("articles").Query();
+            var response = await query.Find();
+            dynamic data = JObject.Parse(response.ResultJson);
+            ViewBag.article = data;
+            return View();
+         }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
