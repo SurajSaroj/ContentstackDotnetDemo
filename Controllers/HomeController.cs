5dfa96aa39991b450c652e3ef24f9690d4a767f7
Suraj Saroj <suraj.saroj3455@gmail.com>
2018-07-16 19:09:32 +0530

    Initial commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,16 +10,18 @@ using Microsoft.Extensions.Options;
 using MVCDemo.Models;
 using MVCDemo;
 using Newtonsoft.Json.Linq;
+using Contentstack.Core.Models;
+
 
 namespace MVCDemo.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ContentstackClient _stack;
-        
+
         public HomeController(ContentstackClient stack, IOptions<ContentstackOptions> contentstackOptions)
-       {
-              _stack = stack;
+        {
+            _stack = stack;
         }
 
         public async Task<IActionResult> Index()
@@ -28,13 +30,31 @@ namespace MVCDemo.Controllers
             var response = await query.Find();
             dynamic data = JObject.Parse(response.ResultJson);
             ViewBag.article = data;
+            ViewBag.articlelength = response.Result.Length;
             return View();
-         }
+        }
+
+        public async Task<IActionResult> News(string id)
+        {
+            Entry entry = _stack.ContentType("articles").Entry(id);
+            await entry.Fetch().ContinueWith((entryResult) =>
+            {
+                dynamic data = JObject.Parse(entryResult.Result.ToJson());
+                ViewBag.ArticleData = data;
+            });
+
+            return View();
+        }
+
+       
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -59,7 +59,10 @@ namespace MVCDemo
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
-            });
+                    routes.MapRoute(
+                    name: "News",
+                    template: "{controller=Home}/{action=News}/{id?}");
+             });
         }
     }
 }
